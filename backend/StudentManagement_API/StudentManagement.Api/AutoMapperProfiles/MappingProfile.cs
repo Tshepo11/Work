b66c7273357a5531f531a51fad47217bc75d57ca
Tshepo Mola <45383774+Tshepo11@users.noreply.github.com>
2022-06-03 12:39:56 +0200

    Add files via upload

---
+++ backend/StudentManagement_API/StudentManagement.Api/AutoMapperProfiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using StudentManagement.Domain.Shared;
+using StudentManagement.Domain.DTOs;
+using StudentManagement.Domain.Entities;
+using StudentManagement.Api.Models;
+
+namespace StudentManagement.Api.AutoMapperProfiles
+{
+    public class MappingProfile : global::AutoMapper.Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<UserDto, UserModel>().ReverseMap();
+        }
+    }
+}
